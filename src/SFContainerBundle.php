5127e91c06e77c2d6d61e315586c70c0fa89403f
Lightman <yaruk.my@gmail.com>
2019-09-02 09:03:28 +0300

    add SFContainerBundle

---
+++ src/SFContainerBundle.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\SFContainer;
+
+use Mrself\Container\Registry\ContainerRegistry;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SFContainerBundle extends Bundle
+{
+    /**
+     * @throws \Mrself\Container\Registry\InvalidContainerException
+     * @throws \Mrself\Container\Registry\NotFoundException
+     * @throws \Mrself\Container\Registry\OverwritingException
+     */
+    public function boot()
+    {
+        $symfonyContainer = $this->container;
+        $appContainer = SymfonyContainer::make();
+        $appContainer->setSymfonyContainer($symfonyContainer);
+        ContainerRegistry::add('App', $appContainer);
+    }
+}
